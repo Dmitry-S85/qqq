0a49fc442279446145ee2cac0c0c8c0916261a55
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-07-07 13:50:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# a = 5
+# while a > 0:
+#     print(a, end=' ')
+#     a -= 1
+
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+k = 234
+while k <= 311:
+    if k % 2 == 0:
+        print(k, '- число четное')
+        k += 1
+    else:
+        print(k, '- число нечетное')
+        k += 1
